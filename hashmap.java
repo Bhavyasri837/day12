f6a7604d3a8a46c80a8996fea53f2f26f5ea80c9
Bhavyasri837 <nsbhavya83@gmail.com>
2024-12-13 15:05:39 +0530

    Add files via upload

---
+++ hashmap.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class hashmap {
+  public static void main(String args[]){
+    HashMap<String,String> a=new HashMap<>();
+    a.put("ano","01");
+    a.put("sname","muraja");
+    System.out.println(a);
+  }
+}
+ 
